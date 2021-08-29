94c0aef7ac5f90dfe877ebe947d9ea3eef27016a
ad2y <ts2188_r@outlook.jp>
2021-08-29 17:47:45 +0900

    MySQL+3

--- env_django_Mysql/web/myprj/settings.py
+++ env_django_Mysql/web/myprj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v49whc_^!h)xxoc4-s-+r+zj47%l%#px&sft2fte^l$95ertld'
+SECRET_KEY = 'django-insecure-xgte5k49h&mete7p%&h$-_eoex0zy@1#6&@b@s$tw9wtbsulf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ DATABASES = {
       'NAME': 'my',
       'USER': 'my',
       'PASSWORD': 'my',
-      'HOST': 'django_db',
+      'HOST': 'django_db_my',
       'PORT': '3306',
     }
 }
